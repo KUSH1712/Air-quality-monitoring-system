ad3cb83b6770b7c9f7046f3e46e62b806d7fe09c
KUSH1712 <kushagra.2428ece2107@kiet.edu>
2025-06-26 00:35:27 +0530

    Added ESP32 gas sensor Arduino code

---
+++ arduino_code/gas_alert_system_arduino_code/gas_alert_system_arduino_code.ino
@@ -0,0 +1,51 @@
+
+
+
+//gas detection simulation on streamlit
+
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "Airtel_anan_6952";
+const char* password = "Air@60773";
+const char* serverURL = "http://192.168.1.9:5000/upload"; 
+
+const int mq135Pin = 34;  // pin definition
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000); Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  int sensorValue = analogRead(mq135Pin);
+  float voltage = sensorValue * (3.3 / 4095.0); // formula to convert voltage value
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverURL);
+    http.addHeader("Content-Type", "application/json");
+
+    String payload = "{\"value\":" + String(voltage, 2) + "}";
+    Serial.print("Sending: ");
+    Serial.println(payload);
+
+    int httpCode = http.POST(payload);
+    Serial.print("HTTP Code: ");
+    Serial.println(httpCode);
+    if (httpCode > 0) {
+      String response = http.getString();
+      Serial.println("Response: " + response);
+    }
+    http.end();
+  }
+
+  delay(5000); // 5000=5sec
+}
+
